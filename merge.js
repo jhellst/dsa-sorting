e02e7a383d12237d420af792d96a8118a56981a2
Nathan Tjahjadi <nathantja@yahoo.com>
2023-11-10 13:29:17 -0600

    merge helper function

--- merge.js
+++ merge.js
@@ -1,5 +1,41 @@
-function merge() {}
+"use strict";
 
-function mergeSort() {}
+/** Given two sorted arrays, return one sorted new array. */
 
-module.exports = { merge, mergeSort};
+function merge(a, b) {
+  const merged = [];
+  let aPointer = 0;
+  let bPointer = 0;
+
+  while (aPointer < a.length && bPointer < b.length) {
+    if (a[aPointer] <= b[bPointer]) {
+      merged.push(a[aPointer]);
+      aPointer += 1;
+    } else {
+      merged.push(b[bPointer]);
+      bPointer += 1;
+    }
+  }
+
+  if (aPointer !== a.length - 1) {
+    for (let i = aPointer; i < a.length; i++) {
+      merged.push(a[i]);
+    }
+  }
+
+  if (bPointer !== b.length - 1) {
+    for (let i = bPointer; i < b.length; i++) {
+      merged.push(b[i]);
+    }
+  }
+
+  return merged;
+}
+
+
+
+
+
+function mergeSort() { }
+
+module.exports = { merge, mergeSort };
